4ac7866c103448d44eda743ecf2d7b9ae58367da
Rafał Kazik <kazik.raf@gmail.com>
2022-02-15 14:36:21 +0100

    get data from json file

--- src/app.js
+++ src/app.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Card from "./components/Card";
 import "./scss/app.scss";
+import data from "./data/db.json";
 
-ReactDOM.render(<Card />, document.querySelector("#root"));
+ReactDOM.render(<Card data={data} />, document.querySelector("#root"));
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,25 +1,32 @@
 import React from "react";
 
-export default class App extends React.Component {
-  render() {
-    return (
+export default class Card extends React.Component {
+  componentDidMount() {
+    console.log(this.props.data.products);
+  }
+
+  productCard = () =>
+    this.props.data.products.map((i) => (
       <section className="product-card">
         <picture className="product-card__picture picture">
           <img
-            src="./images/mobilePhoto.png"
+            src={i.productImage}
             className="picture__product-image"
             alt="product photo"
           ></img>
         </picture>
         <section className="product-card__content content">
           <div className="content__tag tag">
-            <p className="tag__text">TAG TEXT</p>
+            <p className="tag__text">{i.tagText}</p>
           </div>
-          <h2 className="content__product-name">0012-DE-Muller</h2>
+          <h2 className="content__product-name">{i.productName}</h2>
           <p className="content__project-text">PROJEKT:</p>
-          <h3 className="content__project-name">Point 145</h3>
+          <h3 className="content__project-name">{i.projectName}</h3>
         </section>
       </section>
-    );
+    ));
+
+  render() {
+    return <>{this.productCard()}</>;
   }
 }
